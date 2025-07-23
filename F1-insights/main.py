582669dd24c9869b4aa5987a96c1d52380184a17
distorsiona <rosario.bregante@usm.cl>
2025-07-23 11:37:03 -0400

    Intento de obtener carreras por piloto y lugar y año

--- F1-insights/main.py
+++ F1-insights/main.py
@@ -6,7 +6,7 @@ def main():
     cliente = OpenF1Client()
 
     print("Obteniendo pilotos de 2024...")
-    df_drivers = cliente.obtener_drivers(year=2024)
+    df_drivers = cliente.obtener_drivers(year=2025)
     print(df_drivers.columns)
     print(df_drivers)
 
@@ -15,8 +15,16 @@ def main():
     # print(df_laps[['driver_number', 'lap_number', 'lap_time']].head())
 
 
-    df_sesiones = cliente.session_yearAndCountry(year=2024, country="Hungary")
+    df_sesiones = cliente.session_yearAndCountry(year=2024, country="Hungary") 
+    print("Esto imprime?")
     print(df_sesiones)
+
+    laps_df = cliente.obtenerLaps_driverYear(driver_number=63, year=2024)
+    positions_df = cliente.obtenerPosicion_driverYear(driver_number=63, year=2024)
+
+
+    print(laps_df.head())
+    print(positions_df.head())
 
 if __name__ == "__main__":
     main()
--- F1-insights/src/openf1_client.py
+++ F1-insights/src/openf1_client.py
@@ -8,8 +8,12 @@ class OpenF1Client:
     def __init__(self):
         self.base_url = BASE_URL
 
-    def obtener_datos(self, endpoint: str, params: dict = {}) -> pd.DataFrame:
+    def obtener_datos(self, endpoint: str, params: dict = None) -> pd.DataFrame:
+        if params is None:
+            params = {}
         url = f"{self.base_url}/{endpoint}"
+        print(f"Llamando a la URL: {url} con parámetros: {params}")
+
         try: 
             response = requests.get(url, params=params)
             response.raise_for_status()  #lanza un error si la respuesta no es exitosa
@@ -19,13 +23,67 @@ class OpenF1Client:
             print(f"Error al obtener datos de {url}: {e}")
             return pd.DataFrame()
         
+        
     def obtener_year_season(self, year: int) -> pd.DataFrame:
         url = "https://api.openf1.org/v1/sessions"
         df_sessions = pd.read_json(url)
         df_filtrado = df_sessions[df_sessions['date_start'].str.startswith(str(year))]
         return df_filtrado.reset_index(drop=True)
     
+    def obtenerSessionKeys_drivers(self, driver_number: int = None, year: int = None) -> pd.DataFrame:
+        params = {}
+        if year:
+            params["year"] = year
+        if driver_number:
+            params["driver_number"] = driver_number
+
+        df = self.obtener_datos("drivers", params)
+        print("AQUIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII***************************+")
+        print(df)
+
+        if "session_name" in df.columns:
+            df = df[df["session_name"]=="Race"]
+
+        print("AQUI VAAAAAAAAAAAA")
+        print(df)
+
+        columnas = ['driver_number', 'full_name', 'team_name', 'session_key']
+        return df[columnas] if all(c in df.columns for c in columnas) else df
+    
+
+    def obtenerLaps_driverYear(self, driver_number: int = None, year: int = None) -> pd.DataFrame:
+        carreras = self.obtenerSessionKeys_drivers(driver_number, year)
+        if carreras.empty or "session_key" not in carreras.columns:
+            print("No se encontraron carreras para el año y/o piloto especificado.")
+            return pd.DataFrame()
         
+        session_keys = carreras["session_key"].unique().tolist()
+        #obtener datos por cada vuelta de la session_key
+        laps = []
+        for key in session_keys:
+            df_lap = self.obtener_datos("laps", {"session_key": key, "driver_number": driver_number})
+            if not df_lap.empty:
+                df_lap["session_key"] = key
+                laps.append(df_lap)
+        return pd.concat(laps, ignore_index=True) if laps else pd.DataFrame()
+
+    def obtenerPosicion_driverYear(self, driver_number: int = None, year: int = None) -> pd.DataFrame:
+        carrera = self.obtenerSessionKeys_drivers(driver_number, year)
+        print(carrera) #######################33
+
+        if carrera.empty or "session_key" not in carrera.columns:
+            print("No se encontraron carreras para el año y/o piloto especificado.")
+            return pd.DataFrame()
+        
+        session_keys = carrera["session_key"].unique().tolist()
+        posiciones = []
+        for key in session_keys:
+            df_pos = self.obtener_datos("positions", {"session_key": key, "driver_number": driver_number})
+            if not df_pos.empty:
+                df_pos["session_key"] = key
+                posiciones.append(df_pos)
+
+        return pd.concat(posiciones, ignore_index=True) if posiciones else pd.DataFrame()
 
     def obtener_drivers(self, driver_numer: int = None,session_key: int = None, year: int = None) -> pd.DataFrame:
         params = {}
